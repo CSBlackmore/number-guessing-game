230343aac2ecfd16d189a9700170d8fe5d97496c
csblackmore <camiloserrano299@gmail.com>
2026-03-30 15:59:50 -0500

    feat: All methods finished and working. The program runs with no issues. PENDING: exceptions handling

--- src/game/GuessingGame.java
+++ src/game/GuessingGame.java
@@ -10,43 +10,69 @@ public class GuessingGame {
     private int remainingAttempts;
     private String playerName;
 
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 20;
     private static final int MAX_ATTEMPTS = 3;
 
     // CONSTRUCTOR
     public GuessingGame() {
         this.userInput = new Scanner(System.in);
         this.randomizer = new Random();
-        this.secretNumer = randomizer.nextInt(100) + 1;
+        this.secretNumer = randomizer.nextInt(MAX_NUMBER - MIN_NUMBER +1) + MIN_NUMBER;
         this.remainingAttempts = MAX_ATTEMPTS;
         this.playerName = "";
     }
+
     public void playGame() {
-        // the loop and number of attempts go here
+        // the loop and number of attempts
         setUpPlayer();
-        
-        askForNumber();
+        boolean userWins = false;
+        while (remainingAttempts > 0) {
+            int guess = askForNumber();
+            if (compareGuess(guess)) {
+                userWins = true;
+                break;
+            } else {
+                remainingAttempts--;
+                System.out.println("Remaining attempts: " + remainingAttempts);
+            }
+        }
+        displayResults(userWins);
+        userInput.close();
+        System.out.println("--- End of the game ---");
     }
+
     public void setUpPlayer() {
         System.out.println("Welcome! What's your name?");
         playerName = userInput.nextLine();
         System.out.println("Well, " + playerName + ". The rules are simple: \n" +
-                "There is a secret number between 1 to 100 and you should guess it.\n" +
+                "There is a secret number between " + MIN_NUMBER + " and " + MAX_NUMBER + ". You should guess it.\n" +
                 "You will have " + MAX_ATTEMPTS + " chances and some hints will be provided.\n" +
-                "Here we go!");
+                "The secret number is ready! Here we go!");
     }
-    public int askForNumber() {
-        System.out.println("The secret number is ready!\n" +
-                "Now, please, enter your guess:");
-        int userGuess = userInput.nextInt();
-        return userGuess;
-    }
-    public boolean compareGuess(int userGuess) {
-        // compares the user's guess to the secret number
-        // returns true if correct or false if not
-        // prints hints
-        return true; // temporary
-    }
-    public void displayResults(boolean won) {
 
+    public int askForNumber() {
+        System.out.println("Now, please, enter your guess:");
+        return userInput.nextInt();
+    }
+
+    public boolean compareGuess(int userGuess) {
+        // prints hints
+        if (userGuess > secretNumer) {
+            System.out.println("Oops! It's lower.");
+        }
+        else if (userGuess < secretNumer){
+            System.out.println("Oops! It's higher.");
+        }
+        // compares the user's guess to the secret number
+        return userGuess == secretNumer;
+    }
+
+    public void displayResults(boolean won) {
+        if (won) {
+            System.out.println("Congratulations, " + playerName + "! You win.");
+        } else {
+            System.out.println("Sorry! The secret number was: " + secretNumer + "\nBetter luck next time!");
+        }
     }
 }
